dd4e1b5f8f9e6f68576699c845c3432445f54dbf
KasperAPedersen <35560445+KasperAPedersen@users.noreply.github.com>
2020-08-06 05:36:57 +0200

    Update
    Added:
    - Scroll effect on navigation
    - Desktop version
    - Welcome page
    
    Remade:
    - Footer
    - The generation of applications, products & servers
    - Header.

--- public/inc/styling/js/index.js
+++ public/inc/styling/js/index.js
@@ -1,55 +1,41 @@
-// --
-let currentlyShownServer = 0;
 
-let servers = [
-    ["ip"],
-    ["ip1"]
-];
 // --
-let currentlyShownProduct = 0;
+document.addEventListener('DOMContentLoaded', function(){
+    resize();
 
-let products = [
-    ["Pakke #1", "Product beskrivelse", "temp.png"],
-    ["Pakke #2", "Product beskrivelse", "temp.png"],
-    ["Pakke #3", "Product beskrivelse", "temp.png"],
-    ["Pakke #4", "Product beskrivelse", "temp.png"],
-    ["Pakke #5", "Product beskrivelse", "temp.png"],
-    ["Pakke #6", "Product beskrivelse", "temp.png"],
-];
-// --
+    setInterval(() => {
+        if(changeShownServerOnInterval) changeShownServer();
+        if(changeShownShopOnInterval) changeShownShop();
+    }, 5000);
+
+    window.addEventListener('resize', () => {
+        resize();
+    });
+});
+
+// -- Generate applications
 let currentlyShownApplication = 0;
+let amountOfApplicationsToGenerate = 1;
+let changeShownApplicationsOnInterval = true;
 
 let applications = [
     ["Politi"],
+    ["Staff"],
+    ["Politi"],
+    ["Staff"],
+    ["Politi"],
+    ["Staff"],
+    ["Politi"],
+    ["Staff"],
     ["Staff"]
 ];
 // --
-document.addEventListener('DOMContentLoaded', function(){
-    generateServers();
-    generateProducts();
-    generateApplications();
-
-    setInterval(() => {
-        changeShownServer();
-    }, 5000);
-});
-
-function changeShownApplication(newValue){
-    document.getElementById(`application${currentlyShownApplication}`).classList = "fadeOut";
-    setTimeout(() => {
-        document.getElementById(`application${currentlyShownApplication}`).style.display = "none";
-        
-        currentlyShownApplication = currentlyShownApplication + newValue < applications.length ? (currentlyShownApplication + newValue < 0 ? applications.length - 1 : currentlyShownApplication + newValue) : currentlyShownApplication = 0;
-
-        document.getElementById(`application${currentlyShownApplication}`).style.display = "block";
-        document.getElementById(`application${currentlyShownApplication}`).classList = "fadeIn";
-    }, 1000);
-}
-
 function generateApplications(){
+    document.getElementById('applications').innerHTML = "";
     for(const [index, application] of applications.entries()) {
         let elem = document.createElement('article');
         elem.id = `application${index}`;
+        elem.classList = "shownApplication";
         elem.innerHTML = 
         `
         <div class="applicationHead">
@@ -74,30 +60,60 @@ function generateApplications(){
             </form>
         </div>
         `;
-        
         document.getElementById('applications').appendChild(elem);
     }
 
-    document.getElementById(`application${currentlyShownApplication}`).style.display = "block";
-    document.getElementById(`application${currentlyShownApplication}`).style.opacity = 1;
+    for(let amountIndex = 0; amountIndex < amountOfApplicationsToGenerate; amountIndex++) {
+        for(const [index, application] of applications.entries()) {
+            if (amountOfApplicationsToGenerate > index) {
+                if(applications[amountIndex] != undefined) {
+                    document.getElementById(`application${index}`).style.display = "inline-block";
+                    document.getElementById(`application${index}`).style.opacity = 1;
+                }
+            }
+        }
+    }
 }
 
-function changeShownProduct(newValue){
-    document.getElementById(`product${currentlyShownProduct}`).classList = "fadeOut";
+function changeShownApplication(newValue){
+    document.getElementById(`application${currentlyShownApplication}`).classList = "fadeOut";
     setTimeout(() => {
-        document.getElementById(`product${currentlyShownProduct}`).style.display = "none";
+        document.getElementById(`application${currentlyShownApplication}`).style.display = "none";
         
-        currentlyShownProduct = currentlyShownProduct + newValue < products.length - 1 ? (currentlyShownProduct + newValue < 0 ? products.length - 1 : currentlyShownProduct + newValue) : currentlyShownProduct = 0;
+        currentlyShownApplication = currentlyShownApplication + newValue < applications.length ? (currentlyShownApplication + newValue < 0 ? applications.length - 1 : currentlyShownApplication + newValue) : currentlyShownApplication = 0;
 
-        document.getElementById(`product${currentlyShownProduct}`).style.display = "block";
-        document.getElementById(`product${currentlyShownProduct}`).classList = "fadeIn";
+        document.getElementById(`application${currentlyShownApplication}`).style.display = "inline-block";
+        document.getElementById(`application${currentlyShownApplication}`).classList = "fadeIn";
     }, 1000);
 }
 
+function changeShownApplicationsWidth(newWidth){
+    let shownServers = document.getElementsByClassName('shownApplication');
+    for(server of shownServers) {
+        server.style.width = `${newWidth}px`;
+    }
+}
+
+// -- Generate shops
+let currentlyShownProduct = 0;
+let amountOfShopsToGenerate = 1;
+let changeShownShopOnInterval = true;
+
+let products = [
+    ["Pakke #1", "Product beskrivelse", "temp.png"],
+    ["Pakke #2", "Product beskrivelse", "temp.png"],
+    ["Pakke #3", "Product beskrivelse", "temp.png"],
+    ["Pakke #4", "Product beskrivelse", "temp.png"],
+    ["Pakke #5", "Product beskrivelse", "temp.png"],
+    ["Pakke #6", "Product beskrivelse", "temp.png"],
+];
+// --
 function generateProducts(){
+    document.getElementById('shop').innerHTML = "";
     for(const [index, product] of products.entries()) {
         let elem = document.createElement('article');
         elem.id = `product${index}`;
+        elem.classList = "shownProduct";
         elem.innerHTML = 
         `
         <div class="productHead">
@@ -118,24 +134,55 @@ function generateProducts(){
         document.getElementById('shop').appendChild(elem);
     }
 
-    document.getElementById(`product${currentlyShownProduct}`).style.display = "block";
-    document.getElementById(`product${currentlyShownProduct}`).style.opacity = 1;
+    for(let amountIndex = 0; amountIndex < amountOfShopsToGenerate; amountIndex++) {
+        for(const [index, shop] of products.entries()) {
+            if (amountOfShopsToGenerate > index) {
+                if(products[amountIndex] != undefined) {
+                    document.getElementById(`product${index}`).style.display = "inline-block";
+                    document.getElementById(`product${index}`).style.opacity = 1;
+                }
+            }
+        }
+    }
 }
 
-function changeShownServer(){
-    document.getElementById(`server${currentlyShownServer}`).classList = "fadeOut";
+function changeShownShop(){
+    document.getElementById(`product${currentlyShownProduct}`).classList = "fadeOut";
     setTimeout(() => {
-        document.getElementById(`server${currentlyShownServer}`).style.display = "none";
-        currentlyShownServer < servers.length - 1 ? currentlyShownServer++ : currentlyShownServer = 0;
-        document.getElementById(`server${currentlyShownServer}`).style.display = "block";
-        document.getElementById(`server${currentlyShownServer}`).classList = "fadeIn";
+        document.getElementById(`product${currentlyShownProduct}`).style.display = "none";
+        currentlyShownProduct < products.length - 1 ? currentlyShownProduct++ : currentlyShownProduct = 0;
+        document.getElementById(`product${currentlyShownProduct}`).style.display = "inline-block";
+        document.getElementById(`product${currentlyShownProduct}`).classList = "fadeIn";
     }, 1000);
 }
 
+function changeShownShopWidth(newWidth){
+    let shownServers = document.getElementsByClassName('shownProduct');
+    for(server of shownServers) {
+        server.style.width = `${newWidth}px`;
+    }
+}
+
+// -- Generate servers
+let amountOfServersToGenerate = 1;
+let currentlyShownServer = 0;
+let changeShownServerOnInterval = true;
+
+let servers = [
+    ["ip"],
+    ["ip1"],
+    ["ip1"]
+];
+
+// --
+
 function generateServers(){
+    document.getElementById('servers').innerHTML = "";
+
     for(const [index, server] of servers.entries()) {
         let elem = document.createElement('article');
         elem.id = `server${index}`;
+        elem.classList = "shownServer";
         elem.innerHTML = 
         `
         <div class="serverBody">
@@ -153,11 +200,92 @@ function generateServers(){
         `;
         document.getElementById('servers').appendChild(elem);
     }
-
-    document.getElementById(`server${currentlyShownServer}`).style.display = "block";
-    document.getElementById(`server${currentlyShownServer}`).style.opacity = 1;
+    
+    for(let amountIndex = 0; amountIndex < amountOfServersToGenerate; amountIndex++) {
+        for(const [index, server] of servers.entries()) {
+            if (amountOfServersToGenerate > index) {
+                if(servers[amountIndex] != undefined) {
+                    document.getElementById(`server${amountIndex}`).style.display = "inline-block";
+                    document.getElementById(`server${amountIndex}`).style.opacity = 1;
+                }
+            }
+        }
+    }
 }
 
-function joinServer(ip) {
-    console.log(ip);
+function changeShownServer(){
+    document.getElementById(`server${currentlyShownServer}`).classList = "fadeOut";
+    setTimeout(() => {
+        document.getElementById(`server${currentlyShownServer}`).style.display = "none";
+        currentlyShownServer < servers.length - 1 ? currentlyShownServer++ : currentlyShownServer = 0;
+        document.getElementById(`server${currentlyShownServer}`).style.display = "inline-block";
+        document.getElementById(`server${currentlyShownServer}`).classList = "fadeIn";
+    }, 1000);
 }
+
+function changeShownServerWidth(newWidth){
+    let shownServers = document.getElementsByClassName('shownServer');
+    for(server of shownServers) {
+        server.style.width = `${newWidth}px`;
+    }
+}
+
+function resize(){
+    // -- Servers
+    changeShownServerOnInterval = false;
+    amountOfServersToGenerate = servers.length;
+    currentlyShownServer = 0;
+    if (window.innerWidth < 720) {
+        amountOfServersToGenerate = 1;
+        changeShownServerOnInterval = true;
+    }
+    generateServers();
+    changeShownServerWidth(amountOfServersToGenerate > 3 ? 330 : ( window.innerWidth > 430 ? 330 : ((window.innerWidth - (amountOfServersToGenerate * 30)) / amountOfServersToGenerate)));
+
+    // -- Shop
+    changeShownShopOnInterval = false;
+    amountOfShopsToGenerate = products.length;
+    currentlyShownProduct = 0;
+    if (window.innerWidth < 720) {
+        document.getElementById('shopIndexer').style.display = "block";
+        amountOfShopsToGenerate = 1;
+        changeShownShopOnInterval = true;
+    } else {
+        document.getElementById('shopIndexer').style.display = "none";
+    }
+    generateProducts();
+    changeShownShopWidth(amountOfShopsToGenerate > 3 ? 330 : ( window.innerWidth > 430 ? 330 : ((window.innerWidth - (amountOfShopsToGenerate * 30)) / amountOfShopsToGenerate)));
+
+    // -- Applications
+    changeShownApplicationsOnInterval = false;
+    amountOfApplicationsToGenerate = applications.length;
+    currentlyShownApplication = 0;
+    if (window.innerWidth < 720) {
+        document.getElementById('applicationIndexer').style.display = "block";
+        amountOfApplicationsToGenerate = 1;
+        changeShownApplicationsOnInterval = true;
+    } else {
+        document.getElementById('applicationIndexer').style.display = "none";
+    }
+    generateApplications();
+    changeShownApplicationsWidth(amountOfApplicationsToGenerate > 3 ? 330 : ( window.innerWidth > 430 ? 330 : ((window.innerWidth - (amountOfApplicationsToGenerate * 30)) / amountOfApplicationsToGenerate)));
+}
+
+let scroller,isScrolling = false;
+function scrollToElement(place){
+    isScrolling = true;
+    let doc = document.getElementById(place);
+    let desiredLocation = doc.offsetTop - 50;
+    scroller = setInterval(() => {
+        if (desiredLocation < window.scrollY) window.scrollTo(0, (window.scrollY-10));
+        if (desiredLocation > window.scrollY) window.scrollTo(0, (window.scrollY+10));
+        if(desiredLocation < (window.scrollY + 10) && desiredLocation >( window.scrollY - 10)) {
+            window.clearInterval(scroller);
+            isScrolling = false;
+        }
+    }, 1);
+}
+
+document.addEventListener('wheel', () => {
+    if (isScrolling) window.clearInterval(scroller);
+});
